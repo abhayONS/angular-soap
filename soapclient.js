e647d0e9d9c460428ca9dd6469675f720a3da9c2
Thomas Kerbrat <kerbrat@outlook.com>
2016-10-13 16:24:30 +0200

    Prevent _sendSoapRequest execution if wsdl is not provided.

--- soapclient.js
+++ soapclient.js
@@ -221,8 +221,11 @@ SOAPClient._onLoadWsdl = function(url, method, parameters, async, callback, req)
 }
 SOAPClient._sendSoapRequest = function(url, method, parameters, async, callback, wsdl)
 {
+  if (!wsdl) { return; }
   // get namespace
-  var ns = (wsdl.documentElement.attributes["targetNamespace"] + "" == "undefined") ? wsdl.documentElement.attributes.getNamedItem("targetNamespace").nodeValue : wsdl.documentElement.attributes["targetNamespace"].value;
+  var ns = (wsdl.documentElement.attributes["targetNamespace"] + "" == "undefined")
+    ? wsdl.documentElement.attributes.getNamedItem("targetNamespace").nodeValue
+    : wsdl.documentElement.attributes["targetNamespace"].value;
   // build SOAP request
   var sr =
     //"<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
